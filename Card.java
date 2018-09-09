9ac63d4f83ff24b38e4e6324c2a7a5ef5320d3a7
txu1 <33266673+txu1@users.noreply.github.com>
2018-09-09 00:33:38 -0400

    done constructors, setters, and getters for Card

---
+++ Card.java
@@ -0,0 +1,25 @@
+public class Card {
+
+	private String suit;
+	private int rank;
+	Card(){
+		suit = "";
+		rank = 0;
+	}
+	Card(String s, int r){
+		suit = s;
+		rank = r;
+	}
+	public String getSuit(){
+		return suit;
+	}
+	public int getRank(){
+		return rank;
+	}
+	public void setSuit(String s) {
+		this.suit = s;
+	}
+	public void setRank(int r) {
+		this.rank = r;
+	}
+}
