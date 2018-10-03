b741d4191b9806fe36cd8e935a1028ebb0ec3e1e
Ajwise <43044385+Ajwise@users.noreply.github.com>
2018-10-02 22:38:17 -0700

    Update Computer.java
    Added smart computer

--- Computer.java
+++ Computer.java
@@ -1,7 +1,10 @@
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Random;
 import java.util.Scanner;
 import java.util.Vector;
+import java.io.*;
+import java.util.ArrayList;;
 
 public class Computer extends Player {
 
@@ -10,17 +13,188 @@ public class Computer extends Player {
    
         
 	public Computer(Card[] mine, boolean diff) {
+		
 		super(mine);
 		hand = mine;
-                smart = diff;
+        smart = diff;
+       
 	}
 
 	 public String askRank() {
-		 	Vector<Integer> choices = new Vector();
+		 
+	
+		 
+		 
+		 if (smart) {
+		 
+		 Vector<String> inPlayerHand = new Vector();
+		 boolean pFished = false;
+	        try{
+	        File file = new File("GoFishTurnData.txt");
+	        Scanner sc = new Scanner(file);
+	       
+	        
+	        while (sc.hasNextLine()) {
+	        	String[] sInput = sc.nextLine().split(",");
+	        
+	        	
+	        	
+	    		//converts letters to rank
+	    		if (sInput[0].equals("A")) {
+	    			sInput[0] = "1";
+	    		}
+	    		
+	    		else if (sInput[0].equals("J")) {
+	    			sInput[0] = "11";
+	    		}
+	    		
+	    		else if (sInput[0].equals("Q")) {
+	    			sInput[0] = "12";
+	    		}
+	    		
+	    		else if (sInput[0].equals("K")) {
+	    			sInput[0] = "13";
+	    		}
+	    		
+	        	
+	        
+	        	if(sInput[1].equals("False") && sInput[2].equals("False") ) {
+	        		inPlayerHand.add(sInput[0]);
+	        		
+	        		pFished = true;
+	        	}
+	        	else if(sInput[1].equals("False") && sInput[2].equals("True") ) {
+	        		inPlayerHand.add(sInput[0]);
+	        		
+	        		pFished = false;
+	        	}
+	        	else if(sInput[1].equals("True") && sInput[2].equals("False") ) {
+	        		
+	        		
+	        	}
+	        	else if(sInput[1].equals("True") && sInput[2].equals("True") ) {
+	        		for(int i = 0; i < inPlayerHand.size();i++) {
+	        			if(inPlayerHand.get(i).equals(sInput[0])) {
+	        				inPlayerHand.remove(i);
+	        				i = inPlayerHand.size();
+	        			}
+	        		}
+	        		
+	        	}
+	    	
+	        }
+	        
+	     
+	        Vector<Integer> smartChoices = new Vector();
+	        
+	        for(int i = 0; i < inPlayerHand.size();i++) {
+	        	for(int j = 0; j < hand.length;j++) {
+	        		if(hand[j] != null) {
+	        			
+	        		if(inPlayerHand.get(i).equals(hand[j].getRank()+"")) {
+	        			
+		        		smartChoices.add((hand[j].getRank()));
+	        		
+		        	}
+	        		}
+	        	}
+	        }
+	        
+	        if(pFished) {
+	        
+	        if(smartChoices.isEmpty()) {
+	        	
+	        	for(int i = 0; i < hand.length;i++) {
+	        		
+	        		int flag = 0;
+	        		for(int j = 0; j < hand.length;j++) {
+	        			if(hand[i]!=null) {
+	        				if(hand[j]!=null) {
+	        			if(hand[i].getRank() == hand[j].getRank() ) {
+	        				
+	        				if (i != j) {
+	        				
+	        					
+	        				smartChoices.add(hand[i].getRank());
+	        				
+	        				}
+	        			}
+	        				}
+	        		}
+	        		}
+	        	}
+	        	
+	        }
+	        
+	        if(smartChoices.isEmpty()) {
+	        	for(int i = 0; i < hand.length; i++) {
+			   		if(hand[i] != null) {
+			   		smartChoices.add(hand[i].getRank());
+			   		
+			   		}
+	        	
+	        }
+	        	
+	        }
+	       
+	        }
+	        
+	        
+	        else {
+	        	
+	        	if(smartChoices.isEmpty()) {
+	        	for(int i = 0; i < hand.length;i++) {
+	        		
+	        		int flag = 0;
+	        		for(int j = 0; j < hand.length;j++) {
+	        			if(hand[i]!=null) {
+	        				if(hand[j]!=null) {
+	        			if(hand[i].getRank() == hand[j].getRank() ) {
+	        				
+	        				if (i != j) {
+	        				
+	        				
+	        				smartChoices.add(hand[i].getRank());
+	        				
+	        				}
+	        			}
+	        				}
+	        		}
+	        		}
+	        	}
+	        	for(int i = 0; i < hand.length; i++) {
+			   		if(hand[i] != null) {
+			   		smartChoices.add(hand[i].getRank());
+			   		
+			   		}
+	        	}
+	        }
+	        
+	        
+	        	
+	        
+	        
+	        }
+	        System.out.println(smartChoices.size()); 	
+ 	        Random rand = new Random();
+ 		   	
+ 		   	int output = rand.nextInt(smartChoices.size());
+ 		   	return smartChoices.get(output)+"";
+	        }
+	        catch(IOException e){
+	            System.out.println("Could not open file");
+	        }
+		 }
+		 
+		 
+		 
+		 
+		 Vector<Integer> choices = new Vector();
 		   	for(int i = 0; i < hand.length; i++) {
 		   		if(hand[i] != null) {
 		   		choices.add(hand[i].getRank());
 		   	}}
+		 	
 			
 		   	Random rand = new Random();
 		   	
